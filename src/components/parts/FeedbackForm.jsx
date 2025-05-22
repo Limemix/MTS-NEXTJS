624e2dbc6bc3e0ef33d23ef50bf6a71537556818
Oleksandr <37941863+Limemix@users.noreply.github.com>
2025-05-22 15:09:02 +0300

    Update FeedbackForm.jsx

--- src/components/parts/FeedbackForm.jsx
+++ src/components/parts/FeedbackForm.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from 'react';
 import axios from 'axios';
-import '@/styles/FeedbackForm.css';
+//import '@/styles/FeedbackForm.css';
 
 function FeedbackForm() {
     const [name, setName] = useState('');
